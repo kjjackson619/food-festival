0c748fe05a1fc0c3b91a1900b55a45f957fc4c71
KJ Jackson <kjjackson619@yahoo.com>
2022-03-16 20:25:19 -0400

    optimization

--- dist/tickets.bundle.js
+++ dist/tickets.bundle.js
@@ -9,13 +9,23 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./assets/js/script.js":
-/*!*****************************!*\
-  !*** ./assets/js/script.js ***!
-  \*****************************/
+/***/ "./assets/js/domMethods.js":
+/*!*********************************!*\
+  !*** ./assets/js/domMethods.js ***!
+  \*********************************/
+/***/ ((module) => {
+
+eval("module.exports = createEl;\n\n//# sourceURL=webpack://food-festival/./assets/js/domMethods.js?");
+
+/***/ }),
+
+/***/ "./assets/js/tickets.js":
+/*!******************************!*\
+  !*** ./assets/js/tickets.js ***!
+  \******************************/
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
-eval("/* provided dependency */ var $ = __webpack_require__(/*! jquery */ \"./node_modules/jquery/dist/jquery.js\");\n__webpack_require__(/*! bootstrap */ \"./node_modules/bootstrap/dist/js/bootstrap.js\");\n\n$(document).ready(function () {\n    function createEl(htmlString, attrs, ...children) {\n        if (typeof htmlString !== \"string\") {\n            throw Error(\"Argument 'htmlString' is required and must be a string\");\n        }\n\n        const el = document.createElement(htmlString);\n\n        if (typeof attrs === \"object\") {\n            for (let key in attrs) {\n                if (key.substring(0, 2) === \"on\") {\n                    el.addEventListener(key.substring(2).toLowerCase(), attrs[key]);\n                } else {\n                    el.setAttribute(key, attrs[key]);\n                }\n            }\n        }\n    }\n});\n\n//# sourceURL=webpack://food-festival/./assets/js/script.js?");
+eval("/* provided dependency */ var $ = __webpack_require__(/*! jquery */ \"./node_modules/jquery/dist/jquery.js\");\n__webpack_require__(/*! bootstrap */ \"./node_modules/bootstrap/dist/js/bootstrap.js\");\nconst createEl = __webpack_require__(/*! ./domMethods */ \"./assets/js/domMethods.js\");\n\n$(document).ready(function () {\n    if (window.location.href.indexOf(\"tickets\") > -1) {\n\n        const purchaseBtn = document.getElementById(\"purchaseBtn\");\n        const purchaseEmail = document.getElementById(\"purchaseEmail\");\n        const modalEl = document.querySelector(\".modal-content\");\n        const modalBodyEl = document.querySelector(\".modal-body\");\n        const modalFooterEl = document.querySelector(\".modal-footer\");\n\n\n        function purchaseTicket() {\n\n            modalEl.removeChild(modalBodyEl)\n            modalEl.removeChild(modalFooterEl)\n\n            modalEl.append(createEl(\"div\", { class: \"modal-body\" },\n                createEl(\"h5\", { class: \"modal-title\" },\n                    `Thanks for requesting a ticket purchase! We will send an email to ${purchaseEmail.value} to complete the order form!`\n                ),\n            ))\n\n        }\n        purchaseBtn.addEventListener(\"click\", purchaseTicket);\n    }\n});\n\n\n//# sourceURL=webpack://food-festival/./assets/js/tickets.js?");
 
 /***/ }),
 
@@ -122,7 +132,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = __webpack_require__("./assets/js/script.js");
+/******/ 	var __webpack_exports__ = __webpack_require__("./assets/js/tickets.js");
 /******/ 	
 /******/ })()
 ;
